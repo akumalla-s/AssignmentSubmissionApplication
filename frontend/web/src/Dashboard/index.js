58df7ad05ebcbb9204a28986a9c8567b43bb11ea
Srinivasa Akumalla <113478192+akumalla-s@users.noreply.github.com>
2023-06-22 19:37:21 -0400

    Created a function to submit assignment

--- frontend/web/src/Dashboard/index.js
+++ frontend/web/src/Dashboard/index.js
@@ -1,17 +1,28 @@
-import React from 'react'
-import { useLocalState } from '../util/useLocalStorage';
+import React from "react";
+import { useLocalState } from "../util/useLocalStorage";
 
 export default function Dashboard() {
   const [jwt, setJwt] = useLocalState("", "jwt");
 
+  function createAssignment() {
+    fetch("api/assignments", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${jwt}`,
+      },
+    })
+      .then((response) => {
+        if (response.status === 200) return response.json();
+      })
+      .then((assignment) => {
+        window.location.href = `/assignments/${assignment.id}`;
+      });
+  }
+
   return (
-    <div>
-      <h1>
-        Hello Wolrd
-      </h1>
-      <div>
-        Jwt Value is {jwt}
-      </div>
+    <div style={{ margin: "2em" }}>
+      <button onClick={() => createAssignment()}>Submit New Assignment</button>
     </div>
-  )
+  );
 }
