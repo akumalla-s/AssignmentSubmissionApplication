f3d64cace8a3913ac7a76357049542a4726e5c77
Srinivasa Akumalla <113478192+akumalla-s@users.noreply.github.com>
2023-06-22 18:13:19 -0400

    Connected a Login Page to a Spring RESTful Endpoint

--- frontend/web/src/App.js
+++ frontend/web/src/App.js
@@ -1,5 +1,3 @@
-import { useEffect } from "react";
-import { useLocalState } from "./util/useLocalStorage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./Dashboard";
 import Homepage from "./Homepage";
@@ -7,29 +5,6 @@ import Login from "./Login";
 import PrivateRoute from "./PrivateRoute";
 
 function App() {
-  const [jwt, setJwt] = useLocalState("", "jwt");
-
-  useEffect(() => {
-    if (!jwt) {
-      const reqBody = {
-        username: "test",
-        password: "test",
-      };
-
-      fetch("api/auth/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(reqBody),
-      })
-        .then((response) => Promise.all([response.json(), response.headers]))
-        .then(([body, headers]) => {
-          setJwt(headers.get("authorization"));
-        });
-    }
-  }, [jwt, setJwt]);
-
   return (
     <Router>
       <Routes>
