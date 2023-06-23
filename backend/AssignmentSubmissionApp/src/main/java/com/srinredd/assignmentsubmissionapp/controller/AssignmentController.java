0e8593c66caaaab6daceda94b2892a90490be99d
Srinivasa Akumalla <113478192+akumalla-s@users.noreply.github.com>
2023-06-23 06:32:07 -0400

    Update AssignmentController.java

--- backend/AssignmentSubmissionApp/src/main/java/com/srinredd/assignmentsubmissionapp/controller/AssignmentController.java
+++ backend/AssignmentSubmissionApp/src/main/java/com/srinredd/assignmentsubmissionapp/controller/AssignmentController.java
@@ -1,9 +1,16 @@
 package com.srinredd.assignmentsubmissionapp.controller;
 
+import java.util.Optional;
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,4 +32,21 @@ public class AssignmentController {
 		return ResponseEntity.ok(newAssignment);
 	}
 
+	@GetMapping("")
+	public ResponseEntity<?> getAssignments(@AuthenticationPrincipal User user) {
+		Set<Assignment> assignmentsByUser = assignmentService.findByUser(user);
+		return ResponseEntity.ok(assignmentsByUser);
+	}
+
+	@GetMapping("{assignmentId}")
+	public ResponseEntity<?> getAssignmentById(@PathVariable Long assignmentId, @AuthenticationPrincipal User user) {
+		Optional<Assignment> assignment = assignmentService.findById(assignmentId);
+		return ResponseEntity.ok(assignment.orElse(new Assignment()));
+	}
+
+	@PutMapping("{assignmentId}")
+	public ResponseEntity<?> updateAssignment(@PathVariable Long assignmentId,@RequestBody Assignment assignment, @AuthenticationPrincipal User user) {
+		Assignment updatedAssignment = assignmentService.save(assignment);
+		return ResponseEntity.ok(updatedAssignment);
+	}
 }
