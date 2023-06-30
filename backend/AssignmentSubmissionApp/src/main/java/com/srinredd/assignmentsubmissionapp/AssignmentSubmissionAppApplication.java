135cde5a39a5889f42c4112e58602c672f473d69
Srinivasa Akumalla <113478192+akumalla-s@users.noreply.github.com>
2023-06-29 23:10:55 -0400

    Migrated project to Intellij idea from eclipse
    contains files related to Intellij IDE

--- backend/AssignmentSubmissionApp/src/main/java/com/srinredd/assignmentsubmissionapp/AssignmentSubmissionAppApplication.java
+++ backend/AssignmentSubmissionApp/src/main/java/com/srinredd/assignmentsubmissionapp/AssignmentSubmissionAppApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AssignmentSubmissionAppApplication {
+public class AssignmentSubmissionAppApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AssignmentSubmissionAppApplication.class, args);
