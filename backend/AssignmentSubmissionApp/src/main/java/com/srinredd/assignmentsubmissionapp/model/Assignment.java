33ca4c7ec198b536289ab2db6caffda1b07204ec
Srinivasa Akumalla <113478192+akumalla-s@users.noreply.github.com>
2023-07-04 04:20:31 -0400

    added name variable in Assignment class

--- backend/AssignmentSubmissionApp/src/main/java/com/srinredd/assignmentsubmissionapp/model/Assignment.java
+++ backend/AssignmentSubmissionApp/src/main/java/com/srinredd/assignmentsubmissionapp/model/Assignment.java
@@ -9,64 +9,72 @@ import javax.persistence.ManyToOne;
 @Entity
 public class Assignment {
 
-	@Id 
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
-	private String status;
-	private String githubUrl;
-	private String branch;
-	private String codeReviewUrl;
-	
-	@ManyToOne(optional = false)
-	private User user;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String status;
+    private String githubUrl;
+    private String branch;
+    private String codeReviewUrl;
+
+    @ManyToOne(optional = false)
+    private User user;
 //	private User assignedTo;
 
-	public User getUser() {
-		return user;
-	}
+    public User getUser() {
+        return user;
+    }
 
-	public void setUser(User user) {
-		this.user = user;
-	}
+    public void setUser(User user) {
+        this.user = user;
+    }
 
-	public long getId() {
-		return id;
-	}
+    public long getId() {
+        return id;
+    }
 
-	public void setId(long id) {
-		this.id = id;
-	}
+    public void setId(long id) {
+        this.id = id;
+    }
 
-	public String getStatus() {
-		return status;
-	}
+    public String getStatus() {
+        return status;
+    }
 
-	public void setStatus(String status) {
-		this.status = status;
-	}
+    public void setStatus(String status) {
+        this.status = status;
+    }
 
-	public String getGithubUrl() {
-		return githubUrl;
-	}
+    public String getGithubUrl() {
+        return githubUrl;
+    }
 
-	public void setGithubUrl(String githubUrl) {
-		this.githubUrl = githubUrl;
-	}
+    public void setGithubUrl(String githubUrl) {
+        this.githubUrl = githubUrl;
+    }
 
-	public String getBranch() {
-		return branch;
-	}
+    public String getBranch() {
+        return branch;
+    }
 
-	public void setBranch(String branch) {
-		this.branch = branch;
-	}
+    public void setBranch(String branch) {
+        this.branch = branch;
+    }
 
-	public String getCodeReviewUrl() {
-		return codeReviewUrl;
-	}
+    public String getCodeReviewUrl() {
+        return codeReviewUrl;
+    }
 
-	public void setCodeReviewUrl(String codeReviewUrl) {
-		this.codeReviewUrl = codeReviewUrl;
-	}
+    public void setCodeReviewUrl(String codeReviewUrl) {
+        this.codeReviewUrl = codeReviewUrl;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
