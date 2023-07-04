e5a215663fe345c9238618f87e429ede55c5ca74
Srinivasa Akumalla <113478192+akumalla-s@users.noreply.github.com>
2023-07-04 12:33:03 -0400

    Update AssignmentView with Bootstrap Form groups and Bagdes

--- frontend/web/src/AssignmentView/index.js
+++ frontend/web/src/AssignmentView/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useLocalState } from "../util/useLocalStorage";
 import ajax from "../Services/fetchService";
+import { Form, Button, Col, Row, Container, Badge, DropdownButton, ButtonGroup, Dropdown } from "react-bootstrap";
 
 export default function AssignmentView() {
   const [jwt, setJwt] = useLocalState("", "jwt");
@@ -35,34 +36,71 @@ export default function AssignmentView() {
   }, []);
 
   return (
-    <div>
-      <h1>Assignment {assignmentId}</h1>
+    <Container className="mt-5">
+      <Row className="d-flex align-items-center">
+        <Col>
+          <h1>Assignment {assignmentId}</h1>
+        </Col>
+        <Col>
+          <Badge pill bg="info" style={{ fontSize: "1em" }}>
+            {assignment.status}
+          </Badge>
+        </Col>
+      </Row>
+
       {assignment ? (
         <>
-          <h2>Status: {assignment.status} </h2>
-          <h3>
-            Github URL:{" "}
-            <input
-              type="url"
-              id="githubUrl"
-              value={assignment.githubUrl}
-              onChange={(e) => updateAssignment("githubUrl", e.target.value)}
-            />
-          </h3>
-          <h3>
-            Branch:{" "}
-            <input
-              type="text"
-              id="branch"
-              value={assignment.branch}
-              onChange={(e) => updateAssignment("branch", e.target.value)}
-            />
-          </h3>
-          <button onClick={() => save()}>Submit Assignment</button>
+          <Form.Group as={Row} className="my-3" controlId="formPlaintextEmail">
+            <Form.Label column sm="3" md="2">
+              Assignment Number:
+            </Form.Label>
+            <Col sm="9" md="8" lg="6">
+              <DropdownButton
+                as={ButtonGroup}
+                id="assignmentName"
+                variant={"info"}
+                title="Assignment 1"
+              >
+                {[1,2,3,4,5,6].map(assignmentNum => <Dropdown.Item eventKey={assignmentNum}>{assignmentNum}</Dropdown.Item>)}
+              </DropdownButton>
+            </Col>
+          </Form.Group>
+          <Form.Group as={Row} className="my-3" controlId="formPlaintextEmail">
+            <Form.Label column sm="3" md="2">
+              Github URL:
+            </Form.Label>
+            <Col sm="9" md="8" lg="6">
+              <Form.Control
+                type="url"
+                id="githubUrl"
+                placeholder="http://github.com/username/repo-name"
+                value={assignment.githubUrl}
+                onChange={(e) => updateAssignment("githubUrl", e.target.value)}
+              />
+            </Col>
+          </Form.Group>
+
+          <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
+            <Form.Label column sm="3" md="2">
+              Branch:
+            </Form.Label>
+            <Col sm="9" md="8" lg="6">
+              <Form.Control
+                type="text"
+                id="branch"
+                placeholder="Type in branch name"
+                value={assignment.branch}
+                onChange={(e) => updateAssignment("branch", e.target.value)}
+              />
+            </Col>
+          </Form.Group>
+          <Button size="lg" onClick={() => save()}>
+            Submit Assignment
+          </Button>
         </>
       ) : (
         <></>
       )}
-    </div>
+    </Container>
   );
 }
